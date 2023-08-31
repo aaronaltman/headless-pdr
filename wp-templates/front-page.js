bb1107bea691d3aaed345b81f02c3d9aa98257f4
aka <aaronkaltman@gmail.com>
2023-08-30 22:53:42 -0400

    no idea

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,12 +1,13 @@
 import { gql } from "@apollo/client";
 import Header from "../components/header";
 import HomePage from "../thefront-main/nextjs/src/pages/home";
-
+import Contact from "../thefront-main/nextjs/src/views/Agency/components/Contact/Contact.js";
 export default function Component() {
 
   return (
     <>
  <HomePage />
+   <Contact />
     </>
   );
 }
