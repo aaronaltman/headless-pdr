16a588d9736d5ae6103e8fa68268512cc75a0484
aka <aaronkaltman@gmail.com>
2023-08-30 22:29:46 -0400

    just destroying the home page

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,10 +1,5 @@
 import { gql } from "@apollo/client";
-import Head from "next/head";
-import Link from "next/link";
 import Header from "../components/header";
-import EntryHeader from "../components/entry-header";
-import Footer from "../components/footer";
-import style from "../styles/front-page.module.css";
 
 export default function Component(props) {
   const { title: siteTitle, description: siteDescription } =
@@ -13,21 +8,7 @@ export default function Component(props) {
 
   return (
     <>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
 
-      <Header
-        siteTitle={siteTitle}
-        siteDescription={siteDescription}
-        menuItems={menuItems}
-      />
-
-      <main className="container">
-        <EntryHeader title="Welcome to the Faust Scaffold Blueprint" />
-      </main>
-
-      <Footer />
     </>
   );
 }
