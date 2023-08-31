f27e24b8f0ad2a899a0b1dff0e964fc1645d1377
aka <aaronkaltman@gmail.com>
2023-08-30 22:49:51 -0400

    LETS GO AARON

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,11 +1,12 @@
 import { gql } from "@apollo/client";
 import Header from "../components/header";
+import HomePage from "../thefront-main/nextjs/src/pages/home";
 
 export default function Component() {
 
   return (
     <>
-
+ <HomePage />
     </>
   );
 }
