bc9d6775bfea5f04f7528912eabdfaf6081e634c
aka <aaronkaltman@gmail.com>
2023-08-30 22:44:39 -0400

    I mean let's see

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,10 +1,7 @@
 import { gql } from "@apollo/client";
 import Header from "../components/header";
 
-export default function Component(props) {
-  const { title: siteTitle, description: siteDescription } =
-    props.data.generalSettings;
-  const menuItems = props.data.primaryMenuItems.nodes;
+export default function Component() {
 
   return (
     <>
